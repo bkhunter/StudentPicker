3d44a90b52d2923bce90460b8d6113472b1fabc0
Ben Hunter <bkhunter@ualberta.ca>
2015-01-18 16:37:04 -0700

    Added test for randomness

--- src/ca/ualberta/cs/studentpicker/test/StudentListTest.java
+++ src/ca/ualberta/cs/studentpicker/test/StudentListTest.java
@@ -66,9 +66,47 @@ public class StudentListTest extends TestCase
 		assertTrue("Student1 Not Removed_size", students.size() == 2);
 		assertFalse("Student1 Not Removed", students.contains(testStudent1));
 		
-		
-		
 	}
-
-
+		
+		
+		public void testChooseStudentList() {
+			
+			StudentList studentList = new StudentList();
+			
+			String studentName1 = "A Student";
+			String studentName2 = "Another Student";
+			String studentName3 = "Also Student";
+			
+			Student testStudent1 = new Student(studentName1);
+			Student testStudent2 = new Student(studentName2);
+			
+			studentList.addStudent(testStudent1);
+			studentList.addStudent(testStudent2);
+			
+			//Testing for 1 student
+			for (int i=0; i<1000; i++) {
+			
+				Student student = studentList.chooseStudent();
+			
+				assertTrue("Student is not Null", student != null);
+			
+				//assertTrue("Didn't choose the right student", student.equals(testStudent1));
+			}
+			
+			Student [] studentArray = {testStudent1, testStudent2};
+			
+			for (int i = 0; i < studentArray.length; i++) {
+				
+				Student targetStudent = studentArray[i];
+				int maxcount = 1000;
+				
+				while(maxcount > 0 && !targetStudent.equals(studentList.chooseStudent())) {
+					
+					maxcount--;
+					
+				}
+				assertTrue("too many iterations", maxcount > 0);
+				
+			}
+		}
 }
