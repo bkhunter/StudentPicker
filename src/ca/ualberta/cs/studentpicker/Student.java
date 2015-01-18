a6725a6b75fa71fec82f16889e12905bc0f026f7
Ben Hunter <bkhunter@ualberta.ca>
2015-01-18 15:42:26 -0700

    Added Student and StudentList

---
+++ src/ca/ualberta/cs/studentpicker/Student.java
@@ -0,0 +1,23 @@
+package ca.ualberta.cs.studentpicker;
+
+
+public class Student
+{
+	protected String studentname;
+	
+	public Student(String studentName)
+	{
+		this.studentname = studentName;
+
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return this.studentname;
+	}
+	
+	
+
+}
---
+++ src/ca/ualberta/cs/studentpicker/StudentList.java
@@ -0,0 +1,31 @@
+package ca.ualberta.cs.studentpicker;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import ca.ualberta.cs.studentpicker.Student;
+
+public class StudentList
+{
+	protected ArrayList<Student> studentList;
+	
+	public StudentList() {
+		this.studentList = new ArrayList<Student>();
+	}
+	
+	public Collection<Student> getStudents() {
+		return this.studentList;
+	}
+	
+	public void addStudent(Student s) {
+		
+		this.studentList.add(s);
+		
+	}
+
+	public void removeStudent(Student student)
+	{
+		this.studentList.remove(student);
+		
+	}
+	
+}
